ba1c518434c330f497d2b49a81230342bb87cfa6
lathoub <4082369+lathoub@users.noreply.github.com>
2022-12-12 21:07:34 +0100

    Delete sony9Pin-http-api directory

--- sony9Pin-http-api/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace sony9Pin_http_api.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
